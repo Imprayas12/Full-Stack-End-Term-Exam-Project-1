4144cc32e45d552499cc79a303a7090f00cf3aaa
prayas gautam <prayas12gautam@gmail.com>
2022-12-10 15:36:59 +0530

    story-button-added

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-import * as fs from '/Users/prayas gautam/Desktop/Full Stack End term project -1/node_modules/fs';
+// import * as fs from '/Users/prayas gautam/Desktop/Full Stack End term project -1/node_modules/fs';
 
-
+var globalData;
 function getFile(filename) {
-    const content = fs.readFileSync(filename,'utf-8').split('\n');
-    console.log(content);
-    document.getElementById('paragraph').innerText = content;
+    const xhr = new XMLHttpRequest();
+    xhr.onload = function() {
+        globalData = this.responseText;
+        console.log(globalData);
+        document.getElementById('card-body').innerHTML = this.responseText;
+    }
+    xhr.open("GET",`${filename}`,true);
+    xhr.send();
 }
 
-getFile('AliceInWonderland.txt');
